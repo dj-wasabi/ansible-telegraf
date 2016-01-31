e2205aa7d8c9e4389b0ab495caae189c8b5cfb7a
Werner Dijkerman <ikben@werner-dijkerman.nl>
2016-01-31 18:41:25 +0100

    Fixed test and added travis.

--- test/integration/default/serverspec/localhost/ansible_telegraf_spec.rb
+++ test/integration/default/serverspec/localhost/ansible_telegraf_spec.rb
@@ -15,7 +15,7 @@ describe 'Telegraf Services' do
 end
 
 describe 'Telegraf Configuration' do
-    describe file('/etc/opt/telegraf/telegraf.conf') do
+    describe file('/etc/telegraf/telegraf.conf') do
         it { should be_file}
         it { should be_owned_by 'root'}
         it { should be_grouped_into 'root'}
