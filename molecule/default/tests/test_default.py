1cb38ca480f80a85b2943ecf89f4c39e3cf6334f
Werner Dijkerman <werner@dj-wasabi.nl>
2018-11-21 20:25:13 +0100

    * Moved the when statements into multiple lines; * Updated the Molecule *.yml file to newer version; * Added a influxdb container, so telegraf will start properly on debian/ubuntu;

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -3,14 +3,13 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('telegraf')
 
 
-def test_telegraf_running_and_enabled(Service, SystemInfo):
+def test_telegraf_running_and_enabled(Service):
     telegraf = Service("telegraf")
     assert telegraf.is_enabled
-    if SystemInfo.distribution == 'centos':
-        assert telegraf.is_running
+    assert telegraf.is_running
 
 
 def test_telegraf_dot_conf(File):
@@ -18,7 +17,20 @@ def test_telegraf_dot_conf(File):
     assert telegraf.user == "telegraf"
     assert telegraf.group == "telegraf"
     assert telegraf.mode == 0o640
+    assert telegraf.contains('interval = "10s"')
     assert telegraf.contains('[[inputs.cpu]]')
+    assert telegraf.contains('percpu = true')
+    assert telegraf.contains('[[outputs.influxdb]]')
+    assert telegraf.contains('["http://influxdb:8086"]')
+    assert telegraf.contains('[[inputs.net]]')
+
+
+def test_telegraf_dot_d_dir(File):
+    telegraf = File("/etc/telegraf/telegraf.d")
+    assert telegraf.user == "root"
+    assert telegraf.group == "root"
+    assert telegraf.mode == 0o755
+    assert telegraf.is_directory
 
 
 def test_telegraf_dot_d(File):
